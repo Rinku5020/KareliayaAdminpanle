a19eaee163fc13451a45cf5cac17df1fc7689354
rinku5020 <5020rinkunimaje@gmail.com>
2025-05-06 18:36:39 +0530

    dash1

--- adminpanle/routes/web.php
+++ adminpanle/routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', [DigitalController::class, 'index'])->name('dashboard');
 Route::get('/store', [DigitalController::class, 'showStore'])->name('store');
 
 Route::get('/display',[DigitalController::class,'showDisplay'])->name('display');
+Route::get('/graphics',[DigitalController::class,'showGraphicsAndVideos'])->name('graphics');
+

6592dfdd53527e07847dca0ac3690446cb184d3a
rinku5020 <5020rinkunimaje@gmail.com>
2025-05-06 12:06:22 +0530

    migration

--- adminpanle/app/Http/Controllers/DigitalController.php
+++ adminpanle/app/Http/Controllers/DigitalController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\admin;
+use App\Models\digital;
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
+class DigitalController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +34,7 @@ class AdminController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(admin $admin)
+    public function show(digital $digital)
     {
         //
     }
@@ -42,7 +42,7 @@ class AdminController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(admin $admin)
+    public function edit(digital $digital)
     {
         //
     }
@@ -50,7 +50,7 @@ class AdminController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, admin $admin)
+    public function update(Request $request, digital $digital)
     {
         //
     }
@@ -58,7 +58,7 @@ class AdminController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(admin $admin)
+    public function destroy(digital $digital)
     {
         //
     }
--- adminpanle/app/Models/digital.php
+++ adminpanle/app/Models/digital.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class admin extends Model
+class digital extends Model
 {
     //
 }

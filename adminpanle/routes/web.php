4fce2a98ea290907050b7e7d908a185cab613675
rinku5020 <5020rinkunimaje@gmail.com>
2025-05-07 18:47:57 +0530

    doneerror

--- adminpanle/routes/web.php
+++ adminpanle/routes/web.php
@@ -8,18 +8,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DigitalController::class, 'index'])->name('dashboard');
 Route::get('/login',[Controller::class, 'showLogin'])->name('showLogin');
 Route::get('/login-success',[Controller::class, 'login'])->name('login');
-
 Route::get('/store', [DigitalController::class, 'showStore'])->name('store');
 Route::get('/template', [DigitalController::class, 'showTemplate'])->name('template');
 Route::get('/layout', [DigitalController::class, 'showLayout'])->name('layout');
-
 Route::get('/display',[DigitalController::class,'showDisplay'])->name('display');
-<<<<<<< HEAD
+Route::get('/graphics',[DigitalController::class,'showGraphicsAndVideos'])->name('graphics');
+Route::get('/addNewStore', [DigitalController::class, 'addNewStore'])->name('newStore');
+
 Route::get('/graphics',[GraphicsController::class,'showGraphicsAndVideos'])->name('graphics');
 Route::get('/addGraphics',[GraphicsController::class,'addGraphicsAndVideos'])->name('addGraphics');
 Route::post('/createGraphics',[GraphicsController::class,'createGraphics'])->name('CreateGraphics');
+Route::get('/tableGraphics/{id}',[GraphicsController::class,'tableGraphics'])->name('tableGraphics');
 
-=======
-Route::get('/graphics',[DigitalController::class,'showGraphicsAndVideos'])->name('graphics');
-Route::get('/addNewStore', [DigitalController::class, 'addNewStore'])->name('newStore');
->>>>>>> 3ffe67db28d35aa201cb087a1564a8bef12cc8ee
+
+
+
+
+
